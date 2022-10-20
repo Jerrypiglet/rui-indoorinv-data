63f28a9bd12a668347619188f63bae49fb935246
Jerrypiglet <jerrypiglet@gmail.com>
2022-10-20 18:28:17 -0400

    tested mitsuba/test_mitsuba_openrooms.py on mac

--- mitsuba/test_mitsuba_openrooms.py
+++ mitsuba/test_mitsuba_openrooms.py
@@ -2,11 +2,13 @@ import sys
 sys.path.append('../lib')
 from pathlib import Path
 import numpy as np
+import copy
 
 # Import the library using the alias "mi"
 import mitsuba as mi
 # Set the variant of the renderer
-mi.set_variant('cuda_ad_rgb')
+# mi.set_variant('cuda_ad_rgb') # Linux + GPU
+mi.set_variant('llvm_ad_rgb') # Mac
 
 import xml.etree.ElementTree as et
 from utils_OR.utils_OR_xml import gen_random_str, transformToXml
@@ -16,10 +18,28 @@ from utils_io import read_cam_params
     loading an OpenRooms scene and transform to Mitsuba 0.6.0 (not 3.0.0)
     https://mitsuba.readthedocs.io/en/latest/src/key_topics/scene_format.html#scene-xml-file-format
 '''
-scene_xml_dir = Path('/home/ruizhu/Documents/Projects/renderOpenRooms/public_re_3/scenes/xml1/scene0552_00_more')
+
+device = 'mbp'
+PATH_HOME = {
+    'mbp': '/Users/jerrypiglet/Documents/Projects/OpenRooms_RAW_loader', 
+    'mm1': '/home/ruizhu/Documents/Projects/OpenRooms_RAW_loader', 
+    'qc': '/usr2/rzh/Documents/Projects/directvoxgorui'
+}[device]
+OR_RAW_ROOT = {
+    'mbp': '/Users/jerrypiglet/Documents/Projects/data', 
+    'mm1': '/newfoundland2/ruizhu/siggraphasia20dataset', 
+    'qc': ''
+}[device]
+
+layout_root = Path(OR_RAW_ROOT) / 'layoutMesh'
+shapes_root = Path(OR_RAW_ROOT) / 'uv_mapped'
+envmaps_root = Path(OR_RAW_ROOT) / 'EnvDataset' # not publicly availale
+
+scene_xml_dir = Path(PATH_HOME) / 'data/openrooms_public_re_2/scenes/xml1/scene0552_00_more'
+
 xml_file = scene_xml_dir / 'mainDiffLight.xml'
 tree = et.parse(xml_file)
-root  = tree.getroot()
+root = copy.deepcopy(tree.getroot())
 root.attrib.pop('verion', None)
 # scene = mi.load_file("../scenes/cbox.xml")
 root.set('version', '0.6.0')
@@ -44,6 +64,23 @@ for shape in root.findall('shape'):
         shape_id += gen_random_str(5)
         shape.set('id', shape_id)
     shape_ids.append(shape_id)
+
+    filename = shape.findall('string')[0]
+    if 'uv_mapped/' in filename.get('value'):
+        obj_path = shapes_root / filename.get('value').split('uv_mapped/')[1]
+        assert obj_path.exists(), str(obj_path)
+        filename.set('value', str(obj_path))
+    if 'layoutMesh' in filename.get('value'):
+        obj_path = layout_root / filename.get('value').split('layoutMesh')[1][1:]
+        assert obj_path.exists(), str(obj_path)
+        filename.set('value', str(obj_path))
+
+for emitter in root.findall('emitter'):
+    filename = emitter.findall('string')[0]
+    if 'EnvDataset' in filename.get('value'):
+        obj_path = envmaps_root / filename.get('value').split('EnvDataset')[1][1:]
+        assert obj_path.exists(), str(obj_path)
+        filename.set('value', str(obj_path))
 
 # sampler: adaptive -> independent
 sensor = root.findall('sensor')[0]
