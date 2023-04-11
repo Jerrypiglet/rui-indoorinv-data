72f9a4bf40a6535c609ac38dfa7b20af6764c76e
Jerrypiglet <jerrypiglet@gmail.com>
2023-04-11 01:56:44 -0700

    0

--- test_scripts/test_open3d.py
+++
@@ -1,4 +0,0 @@
-import open3d as o3d
-mesh = o3d.geometry.TriangleMesh.create_sphere()
-mesh.compute_vertex_normals()
-o3d.visualization.draw(mesh, raw_mode=True)
--- tools/synthetic_render/import_xml.py
+++
@@ -1,7 +0,0 @@
-import bpy
-import os
-
-bpy.ops.wm.save_as_mainfile(filepath=os.path.abspath('./test.blend'))
-bpy.ops.wm.open_mainfile(filepath=os.path.abspath('./test.blend'))
-bpy.ops.import_scene.mitsuba(filepath='test.xml', override_scene=True)
-bpy.ops.wm.save_mainfile()
